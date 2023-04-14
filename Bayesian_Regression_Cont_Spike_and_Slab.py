a60fb5c15b2ab74e654cee280a27291eff0c1732
mm2319 <mm2319@ic.ac.uk>
2023-04-14 13:28:22 +0100

    1

--- Bayesian_Regression_Cont_Spike_and_Slab.py
+++ Bayesian_Regression_Cont_Spike_and_Slab.py
@@ -12,14 +12,14 @@ def Bayesian_regression_conti_spike_slab(Y_1, X_1, size_fun_lib):
         mu_hat = 0
         sigma_hat = 10
         tau = 5
-        lambda_hat = pm.Normal('lambda_hat', mu = mu_hat, sd = sigma_hat, shape = size_fun_lib)
-        spike_raw = pm.Normal('spike_raw', mu = 0, sd = 1, shape = size_fun_lib)
+        lambda_hat = pm.Normal('lambda_hat', mu = mu_hat, sigma = sigma_hat, shape = size_fun_lib)
+        spike_raw = pm.Normal('spike_raw', mu = 0, sigma = 1, shape = size_fun_lib)
         pn_1 = pm.Deterministic('spike',tau*spike_raw*pm.invlogit(lambda_hat))
         sigma = pm.Gamma('sigma',1.,0.1,shape=1)
         z_1  = pm.Laplace('z_1', mu=0., b=10., shape=size_fun_lib)        
         beta_1 = pm.Deterministic('beta_1', z_1*pn_1)
         mu_1 = pm.Deterministic(name="mu_1", var = pm.math.matrix_dot(X_1,beta_1))
-        Y_obs_1 = pm.Normal('Y_obs_1', mu=mu_1, sd = sigma, observed = Y1)
+        Y_obs_1 = pm.Normal('Y_obs_1', mu=mu_1, sigma = sigma, observed = Y1)
     with basic_model: 
         trace_rh = pm.sample(4000, tune=2000, cores=1, random_seed=1,init="adapt_diag")
     with basic_model:
